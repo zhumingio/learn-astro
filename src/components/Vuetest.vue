146e2d0e3ca582162d5ee73d5aab8437911f45f1
Zhu Ming <zhumingnet@163.com>
2025-06-05 16:09:51 +0800

    引入vue、svelte，优化css

---
+++ src/components/Vuetest.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="hello" v-for="(item, id) in hello">
+        {{ item }}
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+const initialHello = ["你好", "Hej", "Hallo", "Hola", "Habari"];
+const hello = ref([...initialHello]);
+
+const shuffleArray = () => {
+  const array = [...initialHello];
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  hello.value = array;
+};
+
+onMounted(() => {
+  shuffleArray();
+});
+</script>
+
+
+
+<style>
+.hello {
+    color: #6c06cc;
+    font-size: 80px;
+}
+</style>
